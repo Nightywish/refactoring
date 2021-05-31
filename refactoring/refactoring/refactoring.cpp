b15039d2d366b4c6afa38513db54ff9b1d963a50
Nightywish <chunyu9655@hotmail.com>
2021-05-31 17:42:52 -0400

    Refactoring

---
+++ refactoring/refactoring/refactoring.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+void save_score(int count)
+{
+
+	ifstream input("best_score.txt");
+	if (!input.is_open())
+	{
+		cout << "error, please do it again";
+		return;
+	}
+	int best_score;
+	input >> best_score;
+
+	ofstream output("best_score.txt");
+	if (!output.is_open())
+	{
+		cout << "unable to read the file";
+		return;
+	}
+
+	if (count < best_score)
+	{
+		output << count;
+	}
+	else
+	{
+		output << best_score;
+	}
+
+
+}
+void print_vector(vector<int> vector)
+{
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << vector[i] << " ";
+	}
+
+}
+void play_game()
+{
+	vector<int> guesses;
+	int count = 0;
+
+	int random = rand() % 70;
+	cout << random << endl;
+	cout << "guess next number" << endl;
+	while (true)
+	{
+		int guess;
+		cin >> guess;
+		count++;
+		guesses.push_back(guess);
+		if (guess == random)
+		{
+			cout << "u got it !" << endl;
+			break;
+		}
+		else if (guess > random)
+		{
+			cout << " too high" << endl;
+		}
+		else
+		{
+			cout << " too low" << endl;
+		}
+
+	}
+	save_score(count);
+	print_vector(guesses);
+}
+int main()
+{
+	srand(time(NULL));
+	int choice;
+	do
+	{
+		cout << "0: Quit " << endl << "1:Play game" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 0:
+			cout << "good bye";
+			break;
+		case 1:
+			play_game();
+			break;
+		}
+	} while (choice != 0);
+
+
+}
+
